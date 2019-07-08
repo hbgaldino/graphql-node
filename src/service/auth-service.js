2f072255e79c6d94d49cf15642a08ddb4ea43929
higor.galdino <hbgaldino@gmail.com>
2019-07-08 10:18:12 -0300

    add groups to user context

--- src/service/auth-service.js
+++ src/service/auth-service.js
@@ -5,20 +5,27 @@ import { AuthenticationError } from 'apollo-server';
 const key = 'cQfTjWnZr4u7x!A%D*G-KaPdSgUkXp2s';
 
 export const authenticate = async ({ username, password }) => {
-  const userData = await executeSQL('SELECT * FROM EMPLOYEE WHERE USERNAME = :1 AND PASSWORD = :2', [username, password]);
+
+  const sql = 'SELECT E.ID, E.FULL_NAME, E.EMAIL, R.ID ID_ROLE, R.NAME ROLE_NAME FROM EMPLOYEE E '
+    + 'JOIN EMPLOYEE_ROLES ER ON(E.ID = ER.ID_EMPLOYEE) '
+    + 'JOIN ROLES R ON(ER.ID_ROLE = R.ID) WHERE E.USERNAME = :1 AND E.PASSWORD = :2';
+
+  const userData = await executeSQL(sql, [username, password]);
 
   if (userData.rows.length === 0)
     throw new AuthenticationError('Invalid Username or Password.');
 
   const userRow = userData.rows[0];
+
   const tokenPayload = {
     id: userRow.ID,
     fullName: userRow.FULL_NAME,
     username: userRow.USERNAME,
-    email: userRow.EMAIL
+    email: userRow.EMAIL,
+    groups: userData.rows.map(row => (row.ROLE_NAME))
   };
 
-  return jwt.sign(tokenPayload, key, { expiresIn: 60000 });
+  return jwt.sign(tokenPayload, key, { expiresIn: 60 });
 }
 
 export const verifyAuthToken = (token) => {
